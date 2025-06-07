36aabbe9d9c67dbc69b06d8fb19a280c1fbd2ed0
isk4 <vgutierrez@protonmail.com>
2025-06-07 16:15:25 -0400

    Load source data into pandas dataframes

--- etl/run.py
+++ etl/run.py
@@ -1,5 +1,9 @@
 import sqlite3
+import pandas as pd
 from pathlib import Path
+
+# Configuración raíz del proyecto
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Apertura de conexión a db
 conn = sqlite3.connect("dw.db")
@@ -9,13 +13,22 @@ cursor = conn.cursor()
 schema_sql = Path("schema/schema.sql").read_text(encoding="utf-8")
 cursor.executescript(schema_sql)
 
+# Carga de data desde el origen
+df_transactions = pd.read_json(Path(BASE_DIR / "sample_analytics_dataset" / "sample_analytics.transactions.json"))
+df_accounts     = pd.read_json(Path(BASE_DIR / "sample_analytics_dataset" / "sample_analytics.accounts.json"))
+df_customers    = pd.read_json(Path(BASE_DIR / "sample_analytics_dataset" / "sample_analytics.customers.json"))
 
+print(df_transactions)
+print(df_accounts)
+print(df_customers)
+
+# Persistencia de inserts
 conn.commit()
 
 # Validaciones db y foreign keys
 cursor.execute("PRAGMA integrity_check;")
 if cursor.fetchone()[0] != "ok":
-    raise RuntimeError("Falló el chequeo de integridad.")
+    raise RuntimeError("Falló el chequeo de integridad de la base de datos.")
 
 cursor.execute("PRAGMA foreign_key_check;")
 violations = cursor.fetchall()
