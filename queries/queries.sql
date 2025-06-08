3f932db07aa47f05bbfd6beb76789d6973315b76
isk4 <vgutierrez@protonmail.com>
2025-06-08 06:58:26 -0400

    Add questions

---
+++ queries/queries.sql
@@ -0,0 +1,23 @@
+-- 1. ¿Cuál es el promedio, mínimo, máximo y desviación estándar del límite de las cuentas de usuarios?
+
+
+-- 2. ¿Cuántos clientes poseen más de una cuenta?
+
+-- 3. ¿Cuál es el monto promedio y el número de transacciones del mes de junio?
+
+-- 4. ¿Cuál es el id de cuenta con la mayor diferencia entre su transacción más alta y más baja?
+
+-- 5. ¿Cuántas cuentas tienen exactamente 3 productos y, además, uno de esos productos es "Commodity"?
+
+-- 6. ¿Cuál es el nombre del cliente que, en total entre todas sus cuentas, ha realizado la mayor 
+    -- cantidad de transacciones de tipo sell?
+
+-- 7. ¿Cuál es el usuario del cliente cuya cuenta tiene entre 10 y 20 transacciones de tipo “buy”,
+    -- y que presenta el promedio de inversión más alto por operación de este tipo?
+-- 8. ¿Cuál es el promedio de transacciones de compra y de venta por acción (campo “symbol”)?
+
+-- 9. ¿Cuáles son los diferentes beneficios que tienen los clientes del tier “Gold”?
+
+-- 10. Obtener la cantidad de clientes por rangos etarios ([10–19], [20–29], etc.), que hayan realizado al menos una
+    -- compra de acciones de “amzn”. La edad debe calcularse como la diferencia entre la fecha de corte 2025-05-16 y
+    -- el campo “birthdate
