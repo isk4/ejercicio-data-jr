e66db9700eae5b7714d84fbb62f99d48b93b559b
isk4 <vgutierrez@protonmail.com>
2025-06-07 21:31:47 -0400

    Implement customers' transformation

--- etl/run.py
+++ etl/run.py
@@ -1,25 +1,31 @@
 import sqlite3
+import json
 from etl import BASE_DIR
-from etl.utils import create_tables, get_dataframes, validate_db
+from etl.utils import (
+    create_tables, 
+    get_dataframes, 
+    validate_db, 
+    format_date, 
+    transform_customers
+)
 
-# Apertura de conexión a db
-conn = sqlite3.connect(BASE_DIR / "dw.db")
-cursor = conn.cursor()
+# # Apertura de conexión a db
+# conn = sqlite3.connect(BASE_DIR / "dw.db")
+# cursor = conn.cursor()
 
-# Implementación schema.sql
-create_tables(cursor)
+# # Implementación schema.sql
+# create_tables(cursor)
 
 # Carga de data desde el origen
 df_transactions, df_accounts, df_customers = get_dataframes()
-print(df_transactions)
-print(df_accounts)
-print(df_customers)
+
+df_customers, df_accounts_per_customer = transform_customers(df_customers)
 
 # Persistencia de inserts
-conn.commit()
+# conn.commit()
 
-# Validaciones db y foreign keys
-validate_db(cursor)
+# # Validaciones db y foreign keys
+# validate_db(cursor)
 
-# Cierre de conexión a db
-conn.close()
+# # Cierre de conexión a db
+# conn.close()
--- etl/utils.py
+++ etl/utils.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from pathlib import Path
 from etl import BASE_DIR
+from datetime import datetime, timezone
 
 # Implementación schema.sql
 def create_tables(cursor):
@@ -25,3 +26,26 @@ def validate_db(cursor):
     violations = cursor.fetchall()
     if violations:
         raise RuntimeError(f"Hubo violaciones de foreign key: {violations}")
+    
+# Manejo de formato de fecha de customers
+def format_date(date_input):
+    if isinstance(date_input, dict):
+        date_ms = int(date_input["$numberLong"])
+        return pd.to_datetime(date_ms, unit="ms", utc=True).date()
+    else:
+        return pd.to_datetime(date_input, utc=True).date()
+
+# Transformación dataframe customers
+def transform_customers(df_customers):
+    # Creación surrogate key
+    df_customers["customer_key"] = df_customers.index + 1
+    # Formateo de fecha
+    df_customers["birthdate"] = df_customers["birthdate"].apply(lambda x: format_date(x["$date"]))
+
+    df_customers = df_customers.rename(columns={"name": "customer_name"})
+
+    # Cuentas por customer y dataframe final
+    df_accounts_per_customer = df_customers[["customer_key", "accounts"]]
+    df_customers = df_customers[["customer_key", "customer_name", "username", "birthdate"]]
+
+    return df_customers, df_accounts_per_customer
