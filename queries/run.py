bc3707c758109ce4cf060797617d5b76acd1a0d2
isk4 <vgutierrez@protonmail.com>
2025-06-08 20:35:46 -0400

    Improve format on queries' output

--- queries/run.py
+++ queries/run.py
@@ -15,20 +15,20 @@ conn = sqlite3.connect(BASE_DIR / "dw.db")
 conn.execute("PRAGMA foreign_keys = ON;")
 queries = Path(BASE_DIR / "queries" / "queries.sql").read_text(encoding="utf-8")
 
-print("Ejecutando consultas...")
+print("Ejecutando consultas...\n")
 statements = [statement.strip() for statement in queries.split(";") if statement.strip()]
 
 regex = re.compile(r'(?is)(.*?)(?=\b(?:select|with)\b)')
 for statement in statements:
-    print(f"\n{'-' * 100}\n")
     match = regex.search(statement)
     question = match.group(1)
 
     print(f"{question}\n")
     df = pd.read_sql_query(statement, conn)
     print(df.to_markdown(index=False))
+    print(f"\n{'-' * 100}\n")
 
-print(f"\n{'-' * 100}\n")
+
 print("- Finalizó la ejecución de consultas SQL")
 print(f"\n{'-' * 100}\n")
 
